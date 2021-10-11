e439513ff4cbdb7f4fa871e93341a7d86f1a8706
sunrabbit123 <qudwls185@naver.com>
2021-10-12 08:44:27 +0900

    add : encode_token, decode_token

--- model/token.py
+++ model/token.py
@@ -18,6 +18,28 @@ class TokenModel:
         """
         self.collect: pymongo.collection.Collection = db["session"]
 
+    fernet: Fernet = Fernet(os.environ["CRYPTO_KEY"])
+
+    @staticmethod
+    def decode_token(token: str) -> str:
+        """
+        TokenModel에서 추가된 각 토큰들을 해석해줍니다.
+
+        return : str
+        """
+        token_bytes: bytes = token.encode("ascii")
+
+        decode_token: str = TokenModel.fernet.decrypt(token_bytes)
+        return decode_token
+
+    @staticmethod
+    def encode_token(sub: str, created_at: str) -> str:
+        token_byte: bytes = TokenModel.fernet.encrypt(
+            (sub + os.environ["DISTINGUISHER"] + created_at).encode("ascii")
+        )
+        token_str: str = token_byte.decode("ascii")
+        return token_str
+
     async def add(self, sub: str) -> bool:
         insert_value: dict[str, Any] = {
             "sub": sub,
@@ -25,15 +47,14 @@ class TokenModel:
             "renew_able_at": datetime.now(timezone("Asia/Seoul")) + timedelta(days=20),
             "expired_at": datetime.now(timezone("Asia/Seoul")) + timedelta(days=30),
         }
-        fernet = Fernet(os.environ["CRYPTO_KEY"])
-        token_byte: bytes = fernet.encrypt(
-            (sub + str(insert_value["created_at"].timestamp()).encode("ascii"))
+
+        token_str: str = TokenModel.encode_token(
+            sub, str(insert_value["created_at"].timestamp())
         )
-        token_str: str = token_byte.decode("ascii")
 
         self.collect.insert(dict(insert_value, {"token": token_str}))
 
-        return token_str
+        return True
 
     async def delete(self, token: str) -> bool:
         self.collect.remove({"token": token})
